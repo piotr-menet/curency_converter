9dcf7f9fcd85b78eeb52f2288cc7e7858f9987a9
Piotr Menet <piotr.menet@gmail.com>
2023-05-04 21:51:20 +0100

    Changing js to have more functions in it

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+// const amountElement = document.querySelector(".js-amount");
+// const formElement = document.querySelector(".js-form");
+// const currencyElement = document.querySelector(".js-currency");
+// const resultElement = document.querySelector(".js-result");
+
+// const rateEUR = 4.953;
+// const rateGBP = 5.953;
+// const rateUSD = 3.657;
+
+// formElement.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   const amount = +amountElement.value;
+//   const currency = currencyElement.value;
+
+//   let result;
+
+//   switch (currency) {
+//     case "Euro":
+//       result = amount / rateEUR;
+//       break;
+//     case "British Pound":
+//       result = amount / rateGBP;
+//       break;
+//     case "American Dolar":
+//       result = amount / rateUSD;
+//       break;
+//   }
+
+//   resultElement.innerHTML = `${amount.toFixed(
+//     2
+//   )} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+// });
+
 const amountElement = document.querySelector(".js-amount");
 const formElement = document.querySelector(".js-form");
 const currencyElement = document.querySelector(".js-currency");
@@ -7,12 +41,24 @@ const rateEUR = 4.953;
 const rateGBP = 5.953;
 const rateUSD = 3.657;
 
-formElement.addEventListener("submit", (event) => {
+init();
+
+function init() {
+  formElement.addEventListener("submit", handleFormSubmit);
+}
+
+function handleFormSubmit(event) {
   event.preventDefault();
 
   const amount = +amountElement.value;
   const currency = currencyElement.value;
+  const result = calculateResult(amount, currency);
+  const formattedResult = formatResult(amount, result, currency);
 
+  setResultElementHTML(formattedResult);
+}
+
+function calculateResult(amount, currency) {
   let result;
 
   switch (currency) {
@@ -27,7 +73,15 @@ formElement.addEventListener("submit", (event) => {
       break;
   }
 
-  resultElement.innerHTML = `${amount.toFixed(
+  return result;
+}
+
+function formatResult(amount, result, currency) {
+  return `${amount.toFixed(2)} PLN = <strong>${result.toFixed(
     2
-  )} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
-});
+  )} ${currency}</strong>`;
+}
+
+function setResultElementHTML(html) {
+  resultElement.innerHTML = html;
+}
