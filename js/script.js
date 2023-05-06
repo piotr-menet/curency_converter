625b18a89179e188ec5342d5e58ab19a9df44628
Piotr Menet <piotr.menet@gmail.com>
2023-05-06 12:17:27 +0100

    Removed let result in calculateResult function, also remove breaks

--- js/script.js
+++ js/script.js
@@ -1,36 +1,3 @@
-// const amountElement = document.querySelector(".js-amount");
-// const formElement = document.querySelector(".js-form");
-// const currencyElement = document.querySelector(".js-currency");
-// const resultElement = document.querySelector(".js-result");
-
-// const rateEUR = 4.953;
-// const rateGBP = 5.953;
-// const rateUSD = 3.657;
-
-// formElement.addEventListener("submit", (event) => {
-//   event.preventDefault();
-
-//   const amount = +amountElement.value;
-//   const currency = currencyElement.value;
-
-//   let result;
-
-//   switch (currency) {
-//     case "Euro":
-//       result = amount / rateEUR;
-//       break;
-//     case "British Pound":
-//       result = amount / rateGBP;
-//       break;
-//     case "American Dolar":
-//       result = amount / rateUSD;
-//       break;
-//   }
-
-//   resultElement.innerHTML = `${amount.toFixed(
-//     2
-//   )} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
-// });
 
 const amountElement = document.querySelector(".js-amount");
 const formElement = document.querySelector(".js-form");
@@ -59,18 +26,18 @@ function handleFormSubmit(event) {
 }
 
 function calculateResult(amount, currency) {
-  let result;
+  
 
   switch (currency) {
     case "Euro":
       result = amount / rateEUR;
-      break;
+      
     case "British Pound":
       result = amount / rateGBP;
-      break;
+      
     case "American Dolar":
       result = amount / rateUSD;
-      break;
+      
   }
 
   return result;
